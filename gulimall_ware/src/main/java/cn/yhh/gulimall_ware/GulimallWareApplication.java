7b9f5dfe4e2087d91bf0cc101327ae1d915e8457
yhh <1755494566@qq.com>
2023-02-08 16:32:22 +0800

    Initial commit

---
+++ gulimall_ware/src/main/java/cn/yhh/gulimall_ware/GulimallWareApplication.java
@@ -0,0 +1,13 @@
+package cn.yhh.gulimall_ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWareApplication.class, args);
+    }
+
+}
---
+++ gulimall_ware/src/test/java/cn/yhh/gulimall_ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yhh.gulimall_ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
